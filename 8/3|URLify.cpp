fe176fafeca76828a064739e61f22c3a43cb43b0
AfrikyanA <78482036+AfrikyanA@users.noreply.github.com>
2021-02-21 23:29:42 +0400

    Add files via upload

---
+++ 8/3|URLify.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+	
+	string inputString;
+	cout << "Enter string: ";
+	getline(cin,inputString);
+	
+	vector<char> oString;
+	
+	for(int i = 0; i < (int)inputString.length(); ++i){
+		
+		if (inputString[i] != ' '){
+			oString.push_back(inputString[i]);
+			
+		}
+		else {
+			oString.push_back('%');
+			oString.push_back('2');
+			oString.push_back('0');
+			}
+	
+	}
+		cout << "Output: ";
+	for(int i = 0; i < (int)oString.size(); ++i)
+		cout << oString[i];
+	
+}
